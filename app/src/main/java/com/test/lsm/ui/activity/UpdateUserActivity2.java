0fb0bd1df7ead880d4930430bf8740f3153e4444
yyyu <13165691974@163.com>
2018-09-19 18:32:00 +0800

    第三期--头像选择修改

--- app/src/main/java/com/test/lsm/ui/activity/UpdateUserActivity2.java
+++ app/src/main/java/com/test/lsm/ui/activity/UpdateUserActivity2.java
@@ -1,5 +1,6 @@
 package com.test.lsm.ui.activity;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
@@ -16,6 +17,10 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
+import com.luck.picture.lib.PictureSelector;
+import com.luck.picture.lib.config.PictureConfig;
+import com.luck.picture.lib.config.PictureMimeType;
+import com.luck.picture.lib.entity.LocalMedia;
 import com.test.lsm.MyApplication;
 import com.test.lsm.R;
 import com.test.lsm.bean.event.OnUserInfoChg;
@@ -35,10 +40,12 @@ import com.yyyu.baselibrary.utils.MySPUtils;
 import com.yyyu.baselibrary.utils.MyToast;
 
 import java.io.File;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.OnClick;
 import de.greenrobot.event.EventBus;
+import pub.devrel.easypermissions.EasyPermissions;
 
 /**
  * 功能：修改用户信息界面2
@@ -141,8 +148,63 @@ public class UpdateUserActivity2 extends LsmBaseActivity {
 
     @OnClick({R.id.iv_user_icon, R.id.tv_user_icon})
     public void selectIcon() {
-        picChoicePop.show(llCon);
+        //picChoicePop.show(llCon);
+        // picChoicePop.show(llCon);
+        String[] perms = {Manifest.permission.CAMERA ,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        if (EasyPermissions.hasPermissions(this, perms)) {
+            toChoicePic();
+        } else {
+            EasyPermissions.requestPermissions(this,
+                    "相機訪問權限和文件寫入權限沒開啓，可能導致部分功能無法使用，是否開啓？",
+                    10003,
+                    perms);
+        }
     }
+
+    private void toChoicePic(){
+
+        PictureSelector.create(this)
+                .openGallery(PictureMimeType.ofImage())
+                .theme(R.style.picture_default_style)// 主题样式设置 具体参考 values/styles   用法：R.style.picture.white.style
+                .maxSelectNum(1)// 最大图片选择数量
+                .minSelectNum(1)// 最小选择数量
+                .imageSpanCount(4)// 每行显示个数
+                .selectionMode(PictureConfig.SINGLE)// 多选 or 单选
+                .previewImage(true)// 是否可预览图片
+                .isCamera(true)// 是否显示拍照按钮
+                .isZoomAnim(true)// 图片列表点击 缩放效果 默认true
+                //.imageFormat(PictureMimeType.PNG)// 拍照保存图片格式后缀,默认jpeg
+                //.setOutputCameraPath("/CustomPath")// 自定义拍照保存路径
+                .enableCrop(true)// 是否裁剪
+                .compress(true)// 是否压缩
+                .synOrAsy(true)//同步true或异步false 压缩 默认同步
+                //.compressSavePath(getPath())//压缩图片保存地址
+                //.sizeMultiplier(0.5f)// glide 加载图片大小 0~1之间 如设置 .glideOverride()无效
+                .glideOverride(160, 160)// glide 加载宽高，越小图片列表越流畅，但会影响列表图片浏览的清晰度
+                .withAspectRatio(1, 1)// 裁剪比例 如16:9 3:2 3:4 1:1 可自定义
+                .hideBottomControls(false)// 是否显示uCrop工具栏，默认不显示
+                .isGif(false)// 是否显示gif图片
+                .freeStyleCropEnabled(true)// 裁剪框是否可拖拽
+                .circleDimmedLayer(false)// 是否圆形裁剪
+                .showCropFrame(false)// 是否显示裁剪矩形边框 圆形裁剪时建议设为false
+                .showCropGrid(false)// 是否显示裁剪矩形网格 圆形裁剪时建议设为false
+                .openClickSound(false)// 是否开启点击声音
+                //.isDragFrame(false)// 是否可拖动裁剪框(固定)
+                //                        .videoMaxSecond(15)
+                //                        .videoMinSecond(10)
+                //.previewEggs(false)// 预览图片时 是否增强左右滑动图片体验(图片滑动一半即可看到上一张是否选中)
+                .cropCompressQuality(90)// 裁剪压缩质量 默认100
+                .minimumCompressSize(100)// 小于100kb的图片不压缩
+                //.cropWH()// 裁剪宽高比，设置如果大于图片本身宽高则无效
+                .rotateEnabled(true) // 裁剪是否可旋转图片
+                .scaleEnabled(true)// 裁剪是否可放大缩小图片
+                .forResult(PictureConfig.CHOOSE_REQUEST);//结果回调onActivityResult code
+
+    }
+
+
+
 
     public static final int PICK_CONTACT_REQUEST = 21;
 
@@ -245,6 +307,19 @@ public class UpdateUserActivity2 extends LsmBaseActivity {
                     number = number.trim().replace(" ","");
                 }
                 tvCstTel.setText(""+number);
+                break;
+            }
+            case PictureConfig.CHOOSE_REQUEST:{
+
+                List<LocalMedia> selectList = PictureSelector.obtainMultipleResult(data);
+
+                if (selectList.size()==0){
+                    MyToast.showLong(this , "請選擇圖片");
+                }else{
+                    String compressPath = selectList.get(0).getCompressPath();
+                    GlidUtils.load(this , ivUserIcon , compressPath);
+                    userUpdateVo.setUSER_IMAGE(MyFileOprateUtils.imgToBase64(compressPath, this));
+                }
                 break;
             }
         }
